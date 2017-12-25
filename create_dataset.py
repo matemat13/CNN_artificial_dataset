3e6ae61bb321176c4aea0cf2ffb346b138bf0263
Matouš Vrba <vrbamato@fel.cvut.cz>
2017-12-25 13:47:18 +0100

    Dunno what I changed, check the commit

--- create_dataset.py
+++ create_dataset.py
@@ -8,22 +8,31 @@ except ImportError:
 import random as rnd
 import numpy as np
 import os
+import cv2
 
 # TODO: Load these parameters from a file or command line
 # Settings for the image generation
-max_bg_im_width = 640
-max_bg_im_height = 480
-minimal_bb_width = 100
-minimal_bb_height = 80
-max_rotation = 0  # in degrees
-max_objects_in_image = 1
+max_bg_im_width = 600
+max_bg_im_height = 600
+minimal_bb_width = 40
+minimal_bb_height = 30
+max_rotation = 35  # in degrees
+max_objects_in_image = 4
 label = 0
 min_contrast_change = 0.25
 max_brightness_change = 0.25
 
+show_images = False
+
 
 def generate_images(backgr_ims, object_ims, n_imgs, out_folder):
     im_it = 0
+    
+    if show_images:
+        window_name = "Bounding box check window"
+        cv2.namedWindow(window_name,cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(window_name, 640, 480)
+    
     # Generate n_rn_imgs of random images
     for rn_it in range(0, n_imgs):
         backgr_im = backgr_ims[rnd.randint(0, len(backgr_ims)-1)]
@@ -41,15 +50,15 @@ def generate_images(backgr_ims, object_ims, n_imgs, out_folder):
 
         for obj_it in range(0, n_objects_in_image):
             object_im = object_ims[rnd.randint(0, len(object_ims)-1)]
-            dr_size = object_im.size
+            ob_size = object_im.size
 
             xpos_min = 0
             ypos_min = 0
 
             width_min = minimal_bb_width
             height_min = minimal_bb_height
-            width_max = min(bg_width, dr_size[0], maximal_bb_width)
-            height_max = min(bg_height, dr_size[1], maximal_bb_height)
+            width_max = min(bg_width, ob_size[0], maximal_bb_width)
+            height_max = min(bg_height, ob_size[1], maximal_bb_height)
 
             rotation = rnd.gauss(0, max_rotation/3)
             if rotation > max_rotation:
@@ -62,17 +71,17 @@ def generate_images(backgr_ims, object_ims, n_imgs, out_folder):
             cur_width  = cur_object_im.size[0]
             cur_height = cur_object_im.size[1]
 
+            # Resize the object image randomly
             min_resize_ratio = max(width_min/cur_width, height_min/cur_height)
             max_resize_ratio = min(width_max/cur_width, height_max/cur_height)
             resize_ratio = rnd.uniform(min_resize_ratio, max_resize_ratio)
 
-            # Resize the object image randomly
             cur_width  = int(np.round(cur_width*resize_ratio))
             cur_height = int(np.round(cur_height*resize_ratio))
             cur_object_im = cur_object_im.resize((cur_width, cur_height))
 
-            xpos_max = bg_size[0] - cur_width
-            ypos_max = bg_size[1] - cur_height
+            xpos_max = bg_width - cur_width
+            ypos_max = bg_height - cur_height
 
             xpos = rnd.randint(xpos_min, xpos_max)
             ypos = rnd.randint(ypos_min, ypos_max)
@@ -90,8 +99,8 @@ def generate_images(backgr_ims, object_ims, n_imgs, out_folder):
 
             # lefttop_pt = (xpos/bg_width, ypos/bg_height)
             # rightbot_pt = ((xpos+cur_width)/bg_width, (ypos+cur_height)/bg_height)
-            cur_center = ((xpos+cur_width/2)/bg_width, (ypos+cur_height/2)/bg_height)
-            cur_bb = (cur_width/bg_height, cur_height/bg_width)
+            cur_center = ((xpos+cur_width/2.0)/bg_width, (ypos+cur_height/2.0)/bg_height)
+            cur_bb = (cur_width/bg_width, cur_height/bg_height)
 
             object_bb = (label, cur_center, cur_bb)
             # object_bb = (label, lefttop_pt, rightbot_pt)
@@ -109,6 +118,23 @@ def generate_images(backgr_ims, object_ims, n_imgs, out_folder):
                 ofname.write("{:d} {:f} {:f} {:f} {:f}\n".format(object_bb[0],
                                                                      object_bb[1][0], object_bb[1][1],
                                                                      object_bb[2][0], object_bb[2][1]))
+        if show_images:
+            cv_image = np.array(final_image)[:, :, ::-1].copy() 
+            img_width = cv_image.shape[1]
+            img_height = cv_image.shape[0]
+            for bb in object_bbs_in_image:
+                cent_x = bb[1][0]*img_width
+                cent_y = bb[1][1]*img_height
+                width  = bb[2][0]*img_width
+                height = bb[2][1]*img_height
+                x1 = int(np.ceil(cent_x - width/2))
+                x2 = int(np.floor(cent_x + width/2))
+                y1 = int(np.ceil(cent_y - height/2))
+                y2 = int(np.floor(cent_y + height/2))
+                cv2.rectangle(cv_image, (x1, y1), (x2, y2), (0,0,255), 5)
+
+            cv2.imshow(window_name, cv_image)
+            k = cv2.waitKey(0)  # 0 - wait forever
 
         im_it = im_it + 1
         print("Finished {:d}/{:d} images".format(im_it, n_imgs))
@@ -123,8 +149,8 @@ def main():
     object_im_fnames = os.listdir(object_im_folder)
     object_im_fnames.sort()
 
-    n_train_imgs = 2000
-    n_test_imgs = 200
+    n_train_imgs = 10000
+    n_test_imgs = 1000
     tr_out_folder = "output/training"
     ts_out_folder = "output/testing"
 
@@ -133,8 +159,12 @@ def main():
     for im_fname in backgr_im_fnames:
         im = Image.open("{:s}/{:s}".format(backgr_im_folder, im_fname))
         if im.size[0] > max_bg_im_width or im.size[1] > max_bg_im_height:
-            ratio = min(max_bg_im_width/im.size[0], max_bg_im_height/im.size[1])
+            ratio = max(max_bg_im_width/im.size[0], max_bg_im_height/im.size[1])
             im = im.resize((int(np.floor(im.size[0]*ratio)), int(np.floor(im.size[1]*ratio))))
+            # crop the image to have the correct aspect ratio
+            tmp = Image.new("RGBA", (max_bg_im_width, max_bg_im_height))
+            tmp.paste(im, (0,0), im)
+            im = tmp
         backgr_ims.append(im)
         print(im_fname, "-", backgr_ims[len(backgr_ims)-1].mode)
 
--- display_dataset.py
+++ display_dataset.py
@@ -6,18 +6,28 @@ import numpy as np
 
 def main():
     # TODO: Load these parameters from a file or command line
-    ds_folder = "output/training"
+    ds_folder = "output/testing"
     ds_fnames = os.listdir(ds_folder)
     ds_fnames.sort()
 
-    print("Loading dataset")
-    labels = dict()
+    tot_imgs = int(len(ds_fnames)/2)
+    print("Showing bounding boxes for {:d} total images".format(tot_imgs))
+
+    window_name = "Dataset bounding box check"
+    cv2.namedWindow(window_name,cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_name, 640, 480)
     for filename in ds_fnames:
         img_name = '.'.join(filename.split('.')[0:-1])
         extension = filename.split('.')[-1]
 
         if extension == "txt":
-            with open("{:s}/{:s}".format(ds_folder, filename), 'r') as infname:
+            continue
+
+        label_exists = False;
+        try:
+            labels = list()
+            # Open the label file
+            with open("{:s}/{:s}.txt".format(ds_folder, img_name), 'r') as infname:
                 for line in infname.readlines():
                     spl = line.split(' ')
                     label = (
@@ -27,30 +37,17 @@ def main():
                         (float(spl[3]),  # width of the bb
                          float(spl[4]))   # height of the bb
                     )
-                    if img_name not in labels:
-                        labels[img_name] = list()
-                    labels[img_name].append(label)
+                    labels.append(label)
 
-    tot_imgs = len(labels)
-    print("Showing bounding boxes for {:d} total images".format(tot_imgs))
+            label_exists = True;
+	except:
+            print("Image without a label: '{:s}'!".format(filename))
 
-    window_name = "Dataset bounding box check"
-    cv2.namedWindow(window_name,cv2.WINDOW_NORMAL)
-    cv2.resizeWindow(window_name, 1920, 1080)
-    for filename in ds_fnames:
-        img_name = '.'.join(filename.split('.')[0:-1])
-        extension = filename.split('.')[-1]
-
-        if extension == "txt":
-            continue
-
-        img = cv2.imread("{:s}/{:s}".format(ds_folder, filename), cv2.IMREAD_UNCHANGED)
-        if not img_name in labels:
-            print("Unknown image without a label: '{:s}'!".format(filename))
-        else:
-            img_width = img.shape[0]
-            img_height = img.shape[1]
-            for label in labels[img_name]:
+        if label_exists:
+            img = cv2.imread("{:s}/{:s}".format(ds_folder, filename), cv2.IMREAD_UNCHANGED)
+            img_width = img.shape[1]
+            img_height = img.shape[0]
+            for label in labels:
                 cent_x = label[1][0]*img_width
                 cent_y = label[1][1]*img_height
                 width  = label[2][0]*img_width
@@ -61,10 +58,13 @@ def main():
                 y2 = int(np.floor(cent_y + height/2))
                 cv2.rectangle(img, (x1, y1), (x2, y2), (0,0,255), 5)
 
-        cv2.imshow(window_name, img)
-        k = cv2.waitKey(0)  # 0 - wait forever
-        if k == ord('w'):
-            print("Image {:s} marked as labeled WRONG!".format(filename))
+	    cv2.imshow(window_name, img)
+	    k = cv2.waitKey(0)  # 0 - wait forever
+	    if k == ord('w'):
+	        print("Image {:s} marked as labeled WRONG!".format(filename))
+            elif k == ord('q'):
+	        print("Ending")
+                break
 
     cv2.destroyWindow(window_name)
     print("Done with everything. Goodbye.")
